c7959b6b28fc100b14ead2c38b4e9fa9d56bdba1
Olian04 <rift95@live.se>
2023-03-16 08:46:46 +0100

    chore(npm): Version 0.4.0

--- src/api.ts
+++ src/api.ts
@@ -9,17 +9,17 @@ export const html = createRenderer<Node>({
     const frag = document.createDocumentFragment();
     return {
       clearChildren: () => frag.replaceChildren(),
-      appendChildren: frag.append,
+      appendChildren: (...els) => frag.append(...els),
       getRenderTarget: () => frag,
     };
   },
   createElement: (tag) => {
     const el = document.createElement(tag);
     return {
-      appendChildren: el.append,
-      setAttribute: el.setAttribute,
-      addEventListener: el.addEventListener,
-      removeEventListener: el.removeEventListener,
+      appendChildren: (...els) => el.append(...els),
+      setAttribute: (k, v) => el.setAttribute(k, v),
+      addEventListener: (ev, cb) => el.addEventListener(ev, cb),
+      removeEventListener: (ev, cb) => el.removeEventListener(ev, cb),
       setClassName: (className) => (el.className = className),
       getRenderTarget: () => el,
     };
